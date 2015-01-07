fe2598067b14af9afcb9b7d81b8ee181d4eb7933
Neal Feierabend <neal@nealf.com>
2015-01-07 15:23:56 -0500

    First commit of VT_PDF_parser.js
    The parser for pulling the data out of the VT Police PDF files is still
    in a non-working condition, but pushing before the next hack night so
    others can work on it if they want.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # safe_walk_blacksburg
-An app designed to improve the safety of walking around Blacksburg, VA
+An app designed to improve the safety of walking around Blacksburg, VA, 
+particularly at night by showing routes that provide sidewalks and street lighting, 
+and including historical crime data overlays.
+
+# Currently a non-working project
+
+### Data sources
+Street lighting and sidewalk data will come from the Town of Blacksburg [GIS Department](http://www.blacksburg.gov/Index.aspx?page=29)
+Crime data will come from 
+*VT Police Department [Crime Log PDFs](http://www.police.vt.edu/VTPD_v2.1/crime_logs.html)
+*Town reports from [CrimeMapping.com](http://www.crimemapping.com/map.aspx?aid=36898f05-f44b-4778-8c73-27f88b794e0c), which the town uses to publish their data
---
+++ data/VT_PDF_parser.js
@@ -0,0 +1,93 @@
+//The purpose of this script is to parse the PDFs of the crime logs of the VT Police Department (http://www.police.vt.edu/VTPD_v2.1/crime_logs.html)
+
+//Runs on node.js and Requires the pdf2json library which can be found in npm (https://github.com/modesty/pdf2json)
+
+var nodeUtil = require("util"), fs = require("fs"), PDFParser = require("pdf2json/pdfparser");
+var pdfParser = new PDFParser();
+
+//The PDF can/should be pulled from the website directly (e.g. http://www.police.vt.edu/VTPD_v2.1/crime_stats/crime_logs/data/VT_2014-12_Crime_Log.pdf)
+//Using a local copy for offline development
+pdfParser.loadPDF('/tmp/VT_2014-12_Crime_Log.pdf');
+
+//Create an empty data structure for the output
+var dataRows = [];
+
+//Adds the previous row to dataRows and sets currentRow to an empty row structure
+function newRow(finishedRow) {
+	emptyRow = {"caseNumber":null, "dateReported":null, "criminalOffense":null, "location":null, "date":null, "time":null, "disposition":null};
+	if (finishedRow) {
+		dataRows.push(finishedRow);
+		console.log("Wrote row " + dataRows.length);
+	}
+	return emptyRow;
+}
+
+var currentRow = null;
+var isNewRow = true;
+
+//Grab the pages in the PDF
+var pages = pdfParser.data.Pages;
+
+//For every page in the PDF, we'll look through each of the Texts arrays to pull out the fields
+for (var i=0; i<pages.length; i++) {
+	var page = pages[i];
+	var texts = page.Texts;
+	
+	//For every text field, we need to parse and decide which column it belongs to
+	for (var j=0; j<texts.length; j++) {
+		textInfo = texts[j];
+		text = decodeURIComponent(textInfo.R[0].T);
+		
+		//Each page contains a bunch of text fields until you get to the column headers that ends with "Disposition"
+		//We want to (probably) start with the field following "Disposition", which should be the case number
+		while (text != "Disposition") { continue; }
+		if (text == "Disposition") { continue; }
+		
+		//Unfortunately, the fields don't break down perfectly, so things we want to keep together might be broken into multiple parts
+		//For example, multi-line fields in the PDF will be different elements in the texts array
+		//While not every item lines up perfectly, we should be able to use the x position of text fields to determine which column it belongs to
+		
+		//Some defaults to base things on
+		//Case # column: x = 8.258
+		//Date Reported: x = 17.444
+		//Criminal Offense: x = 27.591
+		//Location: x = 60.072
+		//Occurence Date: x = 86.281
+		//Occurence Time: x = 96.429
+		//Disposition: x = 111.419
+		
+		if (textInfo.x < 10.0) {
+			//Case # column
+			if(isNewRow) {
+				currentRow = newRow(currentRow);
+				isNewRow = false;
+			} else { currentRow.caseNumber += " " + text }
+			continue;
+		} else if (textInfo.x < 20.0) {
+			//Date Reported column
+			if (currentRow.dateReported) { currentRow.dateReported += " " + text;
+			} else { currentRow.dateReported = text; }
+		} else if (textInfo.x < 30.0) {
+			//Criminal Offense column
+			if (currentRow.criminalOffense) { currentRow.criminalOffense += " " + text;
+			} else { currentRow.criminalOffense = text; }
+		} else if (textInfo.x < 62.0) {
+			//Location column
+			if (currentRow.location) { currentRow.location += " " + text;
+			} else { currentRow.location = text; }
+		} else if (textInfo.x < 90.0) {
+			//Occurence date column
+			if (currentRow.date) { currentRow.date += " " + text;
+			} else { currentRow.date = text; }
+		} else if (textInfo.x < 98.0) {
+			//Occurence time column
+			if (currentRow.time) { currentRow.time += " " + text;
+			} else { currentRow.time = text; }
+		} else if (textInfo.x > 105) {
+			//Disposition column
+			if (currentRow.disposition) { currentRow.disposition += " " + text;
+			} else { currentRow.disposition = text; }
+		}		
+		isNewRow = true; //Everything except a case # will reach here, so that each time we hit a new case # column we can know it is a new row
+	}
+}
